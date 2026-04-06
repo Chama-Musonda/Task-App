801be528a7afea5fe79a656ee681c5574377b02a
Roy-Musonda <roychama28@gmail.com>
2026-04-06 16:34:51 +0000

    scafold project and implement header

---
+++ task-app/src/components/SideBar.tsx
@@ -0,0 +1,13 @@
+import { Clipboard } from "lucide-react"
+
+const SideBar = () => {
+  return (
+    <div className="bg-surface-low">
+      <span>
+        <Clipboard />
+      </span>
+    </div>
+  )
+}
+
+export default SideBar

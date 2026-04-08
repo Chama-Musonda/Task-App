8ddd42baeac2932460ae509e52fc77d0c13c90d2
Roy-Musonda <roychama28@gmail.com>
2026-04-08 08:25:15 +0000

    add dashboard skeleton

--- task-app/src/components/Header.tsx
+++ task-app/src/components/Header.tsx
@@ -2,7 +2,7 @@ import SearchBar from "./SearchBar"
 
 const Header = () => {
   return (
-    <header className="flex justify-between font-display items-center">
+    <header className="flex justify-between font-display items-center col-span-4 row-span-1 mb-8">
       {/* Logo */}
       <div className="mr-4  min-w-[30%]">
         <h1 className="text-xl font-semibold text-primary">Task App</h1>
--- task-app/src/components/SearchBar.tsx
+++ task-app/src/components/SearchBar.tsx
@@ -2,7 +2,7 @@ import { Search } from "lucide-react"
 
 const SearchBar = () => {
   return (
-    <div className="bg-surface-low flex justify-center items-center rounded-full min-h-8 h-[80%] ml-8 p-2 max-w-[120px]">
+    <div className="bg-surface-low flex justify-center items-center rounded-full min-h-8 h-[80%] ml-8 p-2 min-w-30">
         <Search className="text-gray-500 w-4 h-4 sm:w-5 sm:h-5"/>
         <input type="text" placeholder="Search task..."/>
     </div>

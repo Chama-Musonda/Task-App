54e0f8356ca5f21493810a1fbd9a659bcbce400e
Roy Musonda <roychama28@gmail.com>
2026-04-15 17:05:53 +0000

    Add DetailsSideBar, DetailsGrid and DatePicker components

---
+++ task-app/src/Pages/details/DetailsGrid.tsx
@@ -0,0 +1,13 @@
+import DetailsSidebar from "./DetailsSidebar";
+import MainBodyContent from "./MainBodyContent";
+
+const DetailsGrid = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <MainBodyContent />
+      <DetailsSidebar />
+    </div>
+  );
+};
+
+export default DetailsGrid;
--- task-app/src/Pages/details/DetailsSidebar.tsx
+++ task-app/src/Pages/details/DetailsSidebar.tsx
@@ -1,9 +1,14 @@
+import AssignedMetadata from "./AssignedMetadata";
 import DatePicker from "./DatePicker";
+import VisualAttachments from "./VisualAttachments";
 
 const DetailsSidebar = () => {
   return (
     <div className="space-y-6">
       <DatePicker />
+      <DetailsSidebar />
+      <AssignedMetadata />
+      <VisualAttachments />
     </div>
   );
 };

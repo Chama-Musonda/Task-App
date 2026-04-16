bcaf5a1fced0db81ce0d8e5e4c9bdc222590bd21
Roy-Musonda <roychama28@gmail.com>
2026-04-16 14:08:18 +0000

    Fixed Infinite recursion bug in DetailsSidebar Compomemt

--- task-app/src/Pages/details/MainBodyContent.tsx
+++ task-app/src/Pages/details/MainBodyContent.tsx
@@ -2,8 +2,9 @@ import DescriptionArea from "./DescriptionArea";
 import SubTaskSection from "./SubtaskSection";
 
 const MainBodyContent = () => {
+  {/* remember to add md:col-span-2*/}
   return (
-    <div className="md:col-span-2 space-y-8">
+    <div className="col-span-2 space-y-8">
       <DescriptionArea />
       <SubTaskSection />
     </div>
